1a7995af7413d0d20be91bf5fb0b8eb4cb72a8f3
Shubham Sharma <shubhamsharma10403@gmail.com>
2025-12-19 05:07:47 +0530

    add rename download and delete logic

--- backend/src/controllers/file.controller.ts
+++ backend/src/controllers/file.controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from 'express';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import s3Client from '../config/s3.js';
 import config from '../config/config.js';
 import File from '../model/file.model.js';
 import { 
+    DeleteObjectCommand, 
+    GetObjectCommand, 
     PutObjectCommand 
 } from '@aws-sdk/client-s3';
 
@@ -48,6 +51,91 @@ const fileController = {
             res.status(500).json({ message: 'Server error during file upload' });
         }
     },
+    
+    getFile: async(req: Request, res: Response) => {
+        try {
+            // @ts-ignore
+            const allFile = await File.find({ userId: req.userId}).sort({ createdAt: -1})
+            res.status(200).json({
+                message: 'Files fetched successfully',
+                files: allFile
+            });
+        } catch (error) {
+            console.error('Get files error:', error);
+            res.status(500).json({ message: 'Server error during fetching files' });
+        }
+    },
+
+    renameFile: async(req: Request, res: Response) => {
+        try {
+            const { id } = req.params;
+            const { newFilename } = req.body;
+            const file = await File.findById(id);
+            if (!file) {
+                res.status(404).json({ message: 'File not found' });
+                return;
+            }
+            file.filename = newFilename;
+            await file.save();
+            res.status(200).json({
+                message: 'File renamed successfully',
+                file
+            });
+        } catch (error) {
+            console.error('Rename file error:', error);
+            res.status(500).json({ message: 'Server error during renaming file' });
+        }
+    },
+
+    deleteFile: async(req: Request, res: Response) => {
+        try {
+            const { id } = req.params;
+            const file = await File.findById(id);
+            if (!file) {
+                res.status(404).json({ message: 'File not found' });
+                return;
+            }
+            // Delete from S3
+            const command = new DeleteObjectCommand({
+                Bucket: config.aws.s3BucketName,
+                Key: file.s3Key
+            })
+            const response = await s3Client.send(command);
+            console.log("Delete response : ", response);
+            const responsedb = await file.deleteOne()
+            console.log("Delete from db: ", responsedb);
+            res.status(200).json({ message: 'File deleted successfully' });
+        } catch (error) {
+            console.error('Delete file error:', error);
+            res.status(500).json({ message: 'Server error during deleting file' });
+        }
+    },
+
+    downloadFile: async (req: Request, res: Response) => {
+        try {
+            const { id } = req.params;
+            // @ts-ignore
+            const file = await File.findOne({ _id: id, userId: req.userId })
+            if(!file) {
+                res.status(404).json({
+                    message: 'File not found'
+                })
+            }
+            const command = new GetObjectCommand({
+                Bucket: config.aws.s3BucketName,
+                Key: file?.s3Key
+            })
+
+            const url = await getSignedUrl(s3Client, command, { expiresIn: 3600 })
+            res.status(200).json({
+                message: 'Download URL generated successfully',
+                url
+            });
+        } catch (error) {
+            console.error('Download file error:', error);
+            res.status(500).json({ message: 'Server error during downloading file' });
+        }
+    }
 }
 
 export default fileController;
