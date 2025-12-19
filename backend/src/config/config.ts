25517c613a4812fda33555752666e5a69fad5314
Shubham Sharma <shubhamsharma10403@gmail.com>
2025-12-19 16:26:55 +0530

    updated config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -17,7 +17,7 @@ const config = {
     google: {
         clientId: process.env.GOOGLE_CLIENT_ID || '',
         clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-        callbackURL: '/api/auth/google/callback'
+        callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/api/auth/google/callback'
     },
 }
 
